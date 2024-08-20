bab4ca5671fa955795216e136649f55e3f16a1b9
thevoxium <thevoxium@gmail.com>
2024-08-20 21:16:44 +0530

    added a test file

---
+++ test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+#include "micrograd.h"
+
+int main() {
+  Value a(3.0);
+
+  Value b(4.0);
+  Value c = a + b;
+  std::cout << "Addition: " << c.str() << std::endl;
+
+  Value d = a * b;
+  std::cout << "Multiplication: " << d.str() << std::endl;
+
+  Value e = a.power(2);
+  std::cout << "Power: " << e.str() << std::endl;
+
+  Value f = Value(-2.0).relu();
+  std::cout << "ReLU: " << f.str() << std::endl;
+
+  Value g = -a;
+  std::cout << "Negation: " << g.str() << std::endl;
+
+  Value h = a - b;
+  std::cout << "Subtraction: " << h.str() << std::endl;
+
+  Value i = a / b;
+  std::cout << "Division: " << i.str() << std::endl;
+
+  Value x(2.0), y(3.0);
+  Value z = x * y;
+  z.backward();
+  std::cout << "Backward (x): " << x.str() << std::endl;
+  std::cout << "Backward (y): " << y.str() << std::endl;
+
+  return 0;
+}
